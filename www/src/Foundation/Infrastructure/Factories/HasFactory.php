add9eb066425bfd18724c9772604e0f35351b331
noshysmiles <39526340+noshysmiles@users.noreply.github.com>
2025-03-09 18:23:38 +0000

    Add Factory elements for application infrastructure

---
+++ www/src/Foundation/Infrastructure/Factories/HasFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Foundation\Infrastructure\Factories;
+
+trait HasFactory
+{
+    /**
+     * Get a new factory instance for the model.
+     *
+     * @param  callable|array|int|null  $count
+     * @param callable|array $state
+     * @return \Foundation\Infrastructure\Factories\DomainFactory<static>
+     */
+    public static function factory($count = null, callable|array $state = []): DomainFactory
+    {
+        $factory = DomainFactory::factoryForModel(get_called_class());
+
+        return $factory
+            ->count(is_numeric($count) ? $count : null)
+            ->state(is_callable($count) || is_array($count) ? $count : $state);
+    }
+}
