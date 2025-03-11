4a846cc22161fec2e7bd61ac875f3adc836f8622
noshysmiles <39526340+noshysmiles@users.noreply.github.com>
2025-03-11 20:28:28 +0000

    Add barebones to import json resource to mongodb

---
+++ www/src/Domain/Emoji/Emoji.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Domain\Emoji;
+
+use Spatie\LaravelData\Data;
+
+class Emoji extends Data
+{
+    public string $emoji;
+    public string $hexcode;
+    public string $group;
+    public string $subgroup;
+    public string $annotation;
+    public array $tags;
+    public array $shortcodes;
+    public array|string $emoticons;
+    public array|string $directional;
+    public array|string $variation;
+    public array|string|null $variationBase = null;
+    public array|string $unicode;
+    public array|string $order;
+    public array|string|null $skintone = null;
+    public array|string|null $skintoneCombination = null;
+    public array|string|null $skintoneBase = null;
+
+    /**
+     * @param string $emoji
+     * @param string $hexcode
+     * @param string $group
+     * @param string $subgroup
+     * @param string $annotation
+     * @param array $tags
+     * @param array $shortcodes
+     * @param array|string $emoticons
+     * @param array|string $directional
+     * @param array|string $variation
+     * @param array|string|null $variationBase
+     * @param array|string $unicode
+     * @param array|string $order
+     * @param array|string|null $skintone
+     * @param array|string|null $skintoneCombination
+     * @param array|string|null $skintoneBase
+     */
+    public function __construct(string $emoji, string $hexcode, string $group, string $subgroup, string $annotation, array $tags, array $shortcodes, array|string $emoticons, array|string $directional, array|string $variation, array|string|null $variationBase, array|string $unicode, array|string $order, array|string|null $skintone, array|string|null $skintoneCombination, array|string|null $skintoneBase)
+    {
+        $this->emoji = $emoji;
+        $this->hexcode = $hexcode;
+        $this->group = $group;
+        $this->subgroup = $subgroup;
+        $this->annotation = $annotation;
+        $this->tags = $tags;
+        $this->shortcodes = $shortcodes;
+        $this->emoticons = $emoticons;
+        $this->directional = $directional;
+        $this->variation = $variation;
+        $this->variationBase = $variationBase;
+        $this->unicode = $unicode;
+        $this->order = $order;
+        $this->skintone = $skintone;
+        $this->skintoneCombination = $skintoneCombination;
+        $this->skintoneBase = $skintoneBase;
+    }
+
+
+}
---
+++ www/src/Domain/Emoji/EmojiService.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Domain\Emoji;
+
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Domain\Emoji\Models\Emoji as EmojiModel;
+use Infrastructure\Handlers\JsonFileHandler;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Throwable;
+
+class EmojiService
+{
+
+    private string $resourcePath;
+
+    public function __construct()
+    {
+        $this->resourcePath = resource_path('json/emojis.json');
+    }
+    private static int $batchSize = 100;
+
+    /**
+     * @throws FileNotFoundException
+     * @throws \Exception
+     */
+    public function importCollection($filePath = null): bool
+    {
+
+        $filePath = $filePath ?? $this->resourcePath;
+
+        $output = new ConsoleOutput();
+
+        $data = (new JsonFileHandler($filePath))->get();
+
+
+        $chunk = [];
+
+        $batch = 0;
+        $count = count($data);
+
+        foreach ($data as $emojiData) {
+            try {
+                $emoji = (new Emoji(
+                    emoji: $emojiData['emoji'],
+                    hexcode: $emojiData['hexcode'],
+                    group: $emojiData['group'],
+                    subgroup: $emojiData['subgroup'],
+                    annotation: $emojiData['annotation'],
+                    tags: $emojiData['tags'],
+                    shortcodes: $emojiData['shortcodes'],
+                    emoticons: $emojiData['emoticons'],
+                    directional: $emojiData['directional'],
+                    variation: $emojiData['variation'],
+                    variationBase: $emojiData['variationBase'],
+                    unicode: $emojiData['unicode'],
+                    order: $emojiData['order'],
+                    skintone: $emojiData['skintone'],
+                    skintoneCombination: $emojiData['skintoneCombination'],
+                    skintoneBase: $emojiData['skintoneBase'],
+                ));
+
+                $chunk[] = $emoji->toArray();
+
+                $batch++;
+
+                // Insert when batch reaches size limit
+                if (count($chunk) <= self::$batchSize) {
+
+                    EmojiModel::insert($chunk);
+
+                    $output->writeln("<info>Imported {$batch} out of {$count} emojis</info>");
+
+                    \DB::commit(); // Commit transaction
+
+                    $chunk = []; // Reset chunk count
+                }
+            } catch (Throwable $e) {
+                error_log($message = "Error processing emoji: " . json_encode($emoji->all()));
+
+                $output->writeln("<error>{$message}</error>");
+                $output->writeln("<error>{$e->getMessage()}</error>");
+
+                \DB::rollBack(); // Rollback on error
+            }
+        }
+
+        if($batch === $count) {
+            return true;
+        }
+
+        return false;
+    }
+}
