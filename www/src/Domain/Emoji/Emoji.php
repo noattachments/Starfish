fe01aac03ea0bdf7fa66695ace2cf9d075de7302
noshysmiles <39526340+noshysmiles@users.noreply.github.com>
2025-04-05 21:51:14 +0100

    Add htmlCode as entity property and hexcode is transformed all uppercase

--- www/src/Domain/Emoji/Emoji.php
+++ www/src/Domain/Emoji/Emoji.php
@@ -8,6 +8,7 @@ class Emoji extends Data
 {
     public string $emoji;
     public string $hexcode;
+    public string $htmlCode;
     public string $group;
     public string $subgroup;
     public string $annotation;
@@ -41,10 +42,11 @@ class Emoji extends Data
      * @param array|string|null $skintoneCombination
      * @param array|string|null $skintoneBase
      */
-    public function __construct(string $emoji, string $hexcode, string $group, string $subgroup, string $annotation, array $tags, array $shortcodes, array|string $emoticons, array|string $directional, array|string $variation, array|string|null $variationBase, array|string $unicode, array|string $order, array|string|null $skintone, array|string|null $skintoneCombination, array|string|null $skintoneBase)
+    public function __construct(string $emoji, string $hexcode, string $htmlCode, string $group, string $subgroup, string $annotation, array $tags, array $shortcodes, array|string $emoticons, array|string $directional, array|string $variation, array|string|null $variationBase, array|string $unicode, array|string $order, array|string|null $skintone, array|string|null $skintoneCombination, array|string|null $skintoneBase)
     {
         $this->emoji = $emoji;
-        $this->hexcode = $hexcode;
+        $this->hexcode = strtoupper($hexcode);
+        $this->htmlCode = '&#'.strtoupper($htmlCode).';';
         $this->group = $group;
         $this->subgroup = $subgroup;
         $this->annotation = $annotation;
